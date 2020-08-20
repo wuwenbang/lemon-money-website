bbfb452ae6684d8cc5224bde5ae1e9cedb8074f2
wuwenbang <wuwenbang@foxmail.com>
2020-08-21 07:10:19 +0800

    deploy

---
+++ precache-manifest.bf76f93aeea19d737269a91ba5bda776.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8fd415fd8411e652a3f",
+    "url": "/lemon-money-website/css/app.9de41e9a.css"
+  },
+  {
+    "revision": "e6a2c9042b1250df56769418951c19d6",
+    "url": "/lemon-money-website/index.html"
+  },
+  {
+    "revision": "a8fd415fd8411e652a3f",
+    "url": "/lemon-money-website/js/app.24e9500f.js"
+  },
+  {
+    "revision": "b0825f43a7b482d299ab",
+    "url": "/lemon-money-website/js/chunk-vendors.8f9ea796.js"
+  },
+  {
+    "revision": "f3aaee380deddea3de78c3de33e81ce5",
+    "url": "/lemon-money-website/manifest.json"
+  },
+  {
+    "revision": "940f55c7967a286f58cdb64477b1d887",
+    "url": "/lemon-money-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/lemon-money-website/robots.txt"
+  }
+]);
